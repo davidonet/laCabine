4c80ef854d17f75e8afdada259fa8ea55d2d4755
David Olivari <david.olivari@gmail.com>
2013-06-05 09:28:34 +0200

    Adding drawing canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 requirejs.config({
 	paths : {
 		underscore : 'lib/underscore',
-		bootstrap : 'lib/bootstrap'
+		bootstrap : 'lib/bootstrap',
+		literallycanvas : 'lib/literallycanvas'
 	},
 	shim : {
 		'underscore' : {
@@ -10,15 +11,38 @@ requirejs.config({
 	}
 });
 
+var myLC;
+
 require(['jquery', 'underscore'], function($, _) {
 	$(function() {
-		$(document).click(function() {
+		$(document).one('click', function() {
 			if (document.documentElement.mozRequestFullScreen)
 				document.documentElement.mozRequestFullScreen();
 		});
+
+		$(document).bind('touchmove', function(e) {
+			if (e.target === document.documentElement) {
+				return e.preventDefault();
+			}
+		});
 		$('.vlauncher').click(function() {
-			$("#vcontainer").fadeOut(2000, function() {
-				$(".centered").fadeIn(1000);
+			$("#vcontainer").fadeOut(1000, function() {
+				$(".centered").fadeIn(1000, function() {
+					setTimeout(function() {
+						$(".centered").fadeOut(500, function() {
+							$("#drawaword").show();
+						});
+					}, 5000)
+				});
+			});
+		});
+		$('#drawaword').hide();
+		require(['literallycanvas'], function() {
+			myLC = $('.literally').literallycanvas({
+				backgroundColor : '#eee',
+				primaryColor : '#333333',
+				imageURLPrefix : '/img',
+				sizeToContainer : false
 			});
 		});
 	});
--- routes/index.js
+++ routes/index.js
@@ -14,13 +14,24 @@ function shuffleArray(array) {
 	return array;
 }
 
+var test_txt = ["mon chien, mon père", "amour touche 2", "style pompier", "russian style"]
+
 exports.index = function(req, res) {
 	fs.readdir('public/cover', function(err, files) {
 		shuffleArray(files);
+		var lFiles = files.slice(0, 6);
+
+		var lData = [];
+		for (var file in lFiles) {
+			lData.push({
+				f : lFiles[file],
+				t : test_txt[Math.floor(Math.random() * test_txt.length)]
+			});
+		}
 		res.render('index', {
 			title : 'Express',
-			img_row1 : files.slice(0,3),
-			img_row2 : files.slice(3,6)
+			img_row1 : lData.slice(0, 3),
+			img_row2 : lData.slice(3, 6)
 		});
 	});
 };
