77611a1030756847334ac9bfb81e075107fc2435
David Olivari <david.olivari@gmail.com>
2013-06-13 11:13:53 +0200

    Complete sequence with bad timer

--- public/js/main.js
+++ public/js/main.js
@@ -26,13 +26,82 @@ require(['jquery', 'underscore'], function($, _) {
 			}
 		});
 		$('.vlauncher').click(function() {
+			$('#counter').text("5");
+			$('#maintxt').text("Vous pouvez maintenant entrer dans la cabine");
+			$('#subtxt').text("N'oubliez pas de laisser trois mots ici en sortant");
 			$("#vcontainer").fadeOut(1000, function() {
-				$(".centered").fadeIn(1000, function() {
-					setTimeout(function() {
-						$(".centered").fadeOut(500, function() {
-							$("#drawaword").show();
-						});
-					}, 5000)
+				$('#counter').css({
+					color : "#E0E4CC"
+				});
+				$("#centered").css({
+					top : 250
+				});
+				$("#centered").fadeIn(100, function() {
+					// Wait before go inside the box
+					var counter = 5;
+					var myInt1 = setInterval(function() {
+						$('#counter').text(counter--);
+						if (counter < 0) {
+							clearInterval(myInt1);
+							$("#centered").fadeOut(500, function() {
+								$('#maintxt').text("La Cabine est en cours d'utilisation");
+								$('#subtxt').text("Patientez jusqu'à la fin du compte à rebours");
+								// Wait 3 minutes for the video
+								counter = 9;
+								$('#counter').text(counter);
+								$("#centered").delay(1000).fadeIn(500);
+
+								var myInt2 = setInterval(function() {
+									$('#counter').text(counter--);
+									if (counter < 0) {
+										clearInterval(myInt2);
+
+										$("#centered").fadeOut(500, function() {
+											$("#centered").css({
+												top : 50
+											});
+											$('#counter').css({
+												color : "#333333"
+											});
+											$('#maintxt').text("Vous venez de voir une vidéo");
+											$('#subtxt').text("Avec le stylo, laissez 3 mots");
+											// Wait for writing
+											counter = 18;
+											$('#counter').text(counter);
+											$("#centered").fadeIn(500);
+											$("#drawaword").fadeIn(500);
+											var myInt3 = setInterval(function() {
+												$('#counter').text(counter--);
+												if (counter < 0) {
+													clearInterval(myInt3);
+													$("#drawaword").fadeOut(500);
+													$("#centered").fadeOut(500, function() {
+														$("#centered").css({
+															top : 250
+														});
+														$('#counter').css({
+															color : "#E0E4CC"
+														});
+														$('#counter').text("Merci");
+														$('#maintxt').html("Vous venez de contribuer au projet transmedia<br/>La Cabine");
+														$('#subtxt').text("Plus d'infos sur le site : www.la-cabine.org");
+														$("#centered").fadeIn(500);
+														setTimeout(function() {
+															$("#centered").fadeOut(500, function() {
+																$("#vcontainer").fadeIn();
+																
+															});
+														}, 3000)
+													});
+												}
+											}, 1000);
+										});
+									}
+								}, 1000);
+							});
+
+						}
+					}, 1000)
 				});
 			});
 		});
